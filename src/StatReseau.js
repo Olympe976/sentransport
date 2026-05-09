6be104dd182824b5cb6106d359ee337427edbb13
Mahuna Olympe <olympeatchatin@gmail.com>
2026-05-09 16:22:19 +0000

    Amélioration Style StatReseau

--- src/StatReseau.js
+++ src/StatReseau.js
@@ -1,22 +1,23 @@
 import './StatReseau.css'
 
-function StatReseau({ nbreLignes, nbreArrets, ligne }){
+function StatReseau({ nbreLignes, nbreArrets, ligne }) {
     return (
         <div className="stat-reseau">
             <div className="stat-container">
-                <span className="stat-nombre">{ nbreLignes }</span>
+                <span className="stat-nombre">{nbreLignes}</span>
                 <span className="stat-libelle">lignes</span>
             </div>
-            
+
             <div className="stat-container">
-                <span className="stat-nombre">{ nbreArrets }</span>
+                <span className="stat-nombre">{nbreArrets}</span>
                 <span className="stat-libelle">arrêts</span>
             </div>
+
             <div className="stat-container">
-                <span className="stat-nombre">{ ligne }</span>
+                <span className="stat-label">Ligne</span>
+                <span className="stat-nombre">{ligne}</span>
                 <span className="stat-libelle">avec le plus d'arrêts</span>
             </div>
-
         </div>
     );
 }
