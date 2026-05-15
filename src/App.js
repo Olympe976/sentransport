0b6938b962e68a1d64af3460b074a0ef673eed92
Mahuna Olympe <olympeatchatin@gmail.com>
2026-05-15 00:05:55 +0000

    Lab5: React consomme Flask via fetch et useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import StatReseau from './StatReseau';
@@ -8,55 +8,35 @@ import DetailLigne from './DetailLigne';
 import Footer from './Footer';
 
 function App() {
-
+  // 1. Les états 
+  const [lignes, setLignes] = useState([]);
+  const [chargement, setChargement] = useState(true);
+  const [erreur, setErreur] = useState(null);
   const [recherche, setRecherche] = useState("");
   const [ligneSelectionnee, setLigneSelectionnee] = useState(null);
   const [nombreRecherches, setNombreRecherches] = useState(0);
 
-  // Tableau de lignes
-  const lignes = [
-    { 
-      id: 1, numero: "1", depart: "Parcelles Assainies", arrivee: "Plateau", arrets: 14 , couleur : "#0A6E31",
-      listeArrets : ["Parcelles U14", "Parcelles U10", "Camberene", "Patte d'Oie", "Grand Dakar", "Colobane", "Ponty", "Plateau"]
-    },
-    { 
-      id: 2, numero: "7", depart: "Guediawaye", arrivee: "Place Obe", arrets: 18, couleur: "#0A4E8A",
-      listeArrets : ["Guediawaye", "Pikine", "Thiaroye", "Keur Massar", "Grand Yoff", "Parcelles", "Liberte 6", "Place Obe"]
-    },
-    { 
-      id: 3, numero: "15", depart: "Pikine", arrivee: "Medina", arrets: 12, couleur: "#8A1A0A",
-      listeArrets : ["Pikine Centre", "Thiaroye Gare", "Hann", "Colobane", "Fass", "Medina"]
-    },
-    { 
-      id: 4, numero: "23", depart: "Ouakam", arrivee: "Grand Dakar", arrets: 10, couleur: "#5C0A8A",
-      listeArrets : ["Ouakam Village", "Mermoz", "Fann", "Point E", "Liberte 5", "Grand Dakar"]
-    },
-    { 
-      id: 5, numero: "8", depart: "Almadies", arrivee: "Colobane", arrets: 16, couleur: "#8A5C0A",
-      listeArrets : ["Almadies", "Ngor", "Yoff", "Ouest Foire", "Liberte 6", "Colobane"]
-    },
-    { 
-      id: 6, numero: "12", depart: "Yoff", arrivee: "Sandaga", arrets: 11, couleur: "#0A6E6E",
-      listeArrets : ["Yoff Village", "Aeroport LSS", "Parcelles U17", "Grand Yoff", "HLM", "Sandaga"]
-    },
-    { 
-      id: 7, numero: "31", depart: "Fann", arrivee: "HLM Grand Yoff", arrets: 11, couleur: "#8A0A4E",
-      listeArrets: ["Fann", "Point E", "Liberte 1", "Sicap Amitie", "Dieuppeul", "HLM", "HLM Grand Yoff"]
-    },
-    { 
-      id: 8, numero: "42", depart: "Liberté 6", arrivee: "Dieuppeul", arrets: 9, couleur: "#1A3D8A",
-      listeArrets: ["Liberte 6", "Liberte 5", "Liberte 2", "Sicap Amitie", "Dieuppeul"]
-    },
-    { 
-      id: 9, numero: "18", depart: "Sicap Mbao", arrivee: "Medina", arrets: 13, couleur: "#6B4D0A",
-      listeArrets: ["Sicap Mbao", "Pikine", "Thiaroye", "Hann", "Colobane", "Fass", "Grand Dakar", "Medina"]
-    },
-    { 
-      id: 10, numero: "27", depart: "Castors", arrivee: "Plateau", arrets: 8, couleur: "#2D0A8A",
-      listeArrets: ["Castors", "Fann", "Mermoz", "Gueule Tapee", "Kermel", "Plateau"]
-    },
-  ];
-
+  
+  //2. Charger les données au démarrage
+  useEffect(() => {
+    fetch("http://localhost:5000/lignes")
+      .then(response => {
+        if(!response.ok){
+          throw new Error (
+            "Erreur serveur : " + response.status 
+          );
+        }
+        return response.json();
+      })
+      .then(data => {
+        setLignes(data);
+        setChargement(false);
+      })
+      .catch(error => {
+        setErreur(error.message);
+        setChargement(false);
+      });
+  }, []);
 
   const totalArrets = () => {
     let total = 0;
@@ -67,6 +47,9 @@ function App() {
   };
 
   const ligneMax = () => {
+    // Le tableau etant vide au premier chargement, on met une valeur ^par defaut
+    if (lignes.length === 0) return { numero: "..." }; 
+
     let max = lignes[0];
 
     lignes.forEach(ligne => {
@@ -94,6 +77,34 @@ function handleClickLigne(ligne){
     setLigneSelectionnee(ligne); //premier clic = sélectionner
   }
 }
+
+  // Ecran de chargement
+  if(chargement){
+    return (
+      <div className="App">
+        <Header/>
+        <main className="contenu">
+          <p className="message-chargement">Chargement des lignes...</p>
+        </main>
+      </div>
+    );
+  }
+
+  // Ecran d'erreur
+  if (erreur){
+    return (
+      <div className="App">
+        <Header/>
+        <main className="contenu">
+          <div className="message-erreur">
+              <p>Impossible de charger les lignes.</p>
+              <p className="erreur-detail">{erreur}</p>
+              <p>Vérifiez que le serveur Flask est lancé (python api/app.py).</p>
+          </div>
+        </main>
+      </div>
+    )
+  }
 
   return (
     <div className="App">
